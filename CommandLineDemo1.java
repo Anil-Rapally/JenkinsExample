51f8139d76c267cd1a9a452d88faf05f503f1631
Anil-Rapally <168633563+Anil-Rapally@users.noreply.github.com>
2025-03-29 12:14:18 +0530

    Add files via upload

---
+++ CommandLineDemo1.java
@@ -0,0 +1,17 @@
+class CommandLineDemo1
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+
+int r1=n1+n2;
+int r2=n1-n2;
+int r3=n1*n2;
+int r4=n1/n2;
+System.out.println(r1);
+System.out.println(r2);
+System.out.println(r3);
+System.out.println(r4);
+}
+}
